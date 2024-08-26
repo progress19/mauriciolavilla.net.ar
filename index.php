9f54ee1a80f72021710d11609e2d4c4bdab6dddf
LoboDev <mauriciolav@gmail.com>
2024-08-26 17:41:00 -0300

    act

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+  <html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mauricio Lavilla">
+    <meta name="generator" content="Mauricio 0.101.0">
+    <title>ML</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <meta name="theme-color" content="#712cf9">
+
+    <!-- Custom styles for this template -->
+    <link href="css.css" rel="stylesheet">
+
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org/",
+      "@type": "Person",
+      "name": "Mauricio Lavilla",
+      "url": "https://mauriciolavilla.ml",
+      "image": "https://avatars.githubusercontent.com/u/4473614?v=4",
+      "sameAs": "https://mauriciolavilla.ml",
+      "jobTitle": "Full Stack Developer"  
+    }
+    </script>
+    
+  </head>
+  <body class="d-flex h-100 text-center text-bg-dark">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+      <main class="div-name">
+        <h1>Mauricio Lavilla</h1>
+        <p class="lead">Full Stack Developer</p>
+        <p><a href="mailto:contacto@mauricio.ml">contacto@mauricio.ml</a></p>
+        (ツ)
+      </main>
+
+      <!--
+      <footer class="mt-auto text-white-50">
+      <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+      </footer>
+      </div>
+      -->
+
+</body>
+</html>
